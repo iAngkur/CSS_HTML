060bfa3ff58c32de6d3d0249709c00098cff5b00
Angkur Mondal <angkurmondal1998@gmail.com>
2024-04-10 14:31:28 +0600

    added some new projects

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -4,13 +4,25 @@ const numberOfDrums = document.querySelectorAll(".drum").length;
 for (let i = 0; i < numberOfDrums; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function (event) {
         let buttonInnerHTML = this.innerHTML;
+        console.log(event);
         makeSound(buttonInnerHTML);
         animateButton(buttonInnerHTML);
     });
 }
 
+// var btn = document.getElementById('w');
+// btn.addEventListener("keypress", function(event) {
+//     console.log(this.id);
+//     this.classList.forEach(className => console.log(className)); // Log each class name individually
+//     makeSound(event.key);
+//     animateButton(event.key);
+// });
+
+
 document.addEventListener("keypress", function(event) {
     console.log(event);
+    // console.log(this.classList);
+    // this.classList.forEach(c => console.log(c) );
     makeSound(event.key);
     animateButton(event.key);
 });
